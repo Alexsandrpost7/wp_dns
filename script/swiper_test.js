4c3c5a44863e7c16ab85ce0a0fb936baedf5bb05
Alexsandrpost7 <76068301+Alexsandrpost7@users.noreply.github.com>
2021-01-28 16:32:38 +0300

    com offer 1/2

--- script/swiper_test.js
+++ script/swiper_test.js
@@ -1,5 +1,6 @@
 
-var mySwiper = new Swiper('.swiper-container', {
+// var mySwiper = new Swiper('.swiper-container' , '.section-grid__offer-swiper-container1' ,  {
+var mySwiper1 = new Swiper('.section-grid__slider-swiper1' ,  {
   // Optional parameters
   // direction: 'vertical',
   direction: 'horizontal',
@@ -17,7 +18,6 @@ var mySwiper = new Swiper('.swiper-container', {
 //     // Показ только активного слайда
 //     limitRotation: true,
 // },
-
 
 
 keyboard: {
@@ -45,6 +45,45 @@ keyboard: {
     el: '.swiper-scrollbar',
     
   },
-})
+});
+
+
+var mySwiper2 = new Swiper('.section-grid__offer-swiper2' ,  {
+  // Optional parameters
+  // direction: 'vertical',
+  direction: 'horizontal',
+  loop: true,
+//     autoplay: {
+//     delay:2000,
+// },
+
+slidesPerView: 3,
+
+keyboard: {
+  enabled: true,
+
+},
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  //   Для перехода по булитам
+    clickable: true,
+    dynamicBullets: true,
+    progressbarOpposite: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    
+  },
+});
 
 //   Если внутри flex? то min-width: 0!!
