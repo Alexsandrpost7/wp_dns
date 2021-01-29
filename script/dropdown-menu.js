8c279e089ad5f5ffe65a496942958cc5e6464321
Alexsandrpost7 <76068301+Alexsandrpost7@users.noreply.github.com>
2021-01-29 15:46:09 +0300

    com jQuery to plug

--- script/dropdown-menu.js
+++ script/dropdown-menu.js
@@ -1,3 +1,15 @@
-$(function() { 
-    $('.section-header-title__city').fadeOut( 'slow' );
- });
+// $(document).ready(function() {
+//     $('.section-header-title__city').fadeOut(4000);
+    
+// });
+
+$(document).ready(function() {
+    $('.section-grid__menu-wr-items').hover(function () {
+        // alert( "Handler for .click() called." ); 
+        //  +ok $('.section-grid__menu-wr-items-dropdown').css('background-color' , 'red');
+        
+        $('.section-grid__menu-wr-items-dropdown').css('display' , 'block');
+
+    });
+    
+});
